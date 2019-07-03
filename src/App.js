fd2ab17bb96501f282c292a1d6d481da8aefc124
Didac <didac@compsaonline.com>
2019-07-03 17:41:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import styles from './App.module.scss';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <main>
+        <section>
+            <h1>Get the freedom you deserve.</h1>
+        </section>
+        <section>
+            <h1>Choose your plan</h1>
+        </section>
+      </main>
     </div>
   );
 }
